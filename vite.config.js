c3d0efd22b177b89eade663495c9ce06d0e2403a
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2025-06-29 00:37:08 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  base: '/Construktor.io/', // Mantenha esta linha para o GitHub Pages
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // <-- Adicione esta seção
     },
   },
 })
